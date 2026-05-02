48ee5ef3bded4f6258ea2ed8ab019aaf566c03c6
GitHub Action <action@github.com>
2026-05-02 20:34:24 +0000

    push

--- JVChat_Premium.user.js
+++ JVChat_Premium.user.js
@@ -5,7 +5,7 @@
 // @namespace      JV_Chat_Custsom_Fork
 // @license        MIT
 // @version        0.2.3.26
-// @icon           https://images.emojiterra.com/google/noto-emoji/unicode-15.1/color/128px/1f7e9.png
+// @icon           https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/128px/2b1b.png
 // @match          http://*.jeuxvideo.com/forums/42-*
 // @match          https://*.jeuxvideo.com/forums/42-*
 // @match          http://*.jeuxvideo.com/forums/1-*
