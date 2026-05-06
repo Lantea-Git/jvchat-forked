8976622afb2a4ddce984bff3c1c160f64167f68a
GitHub Action <action@github.com>
2026-05-06 23:04:42 +0000

    clean payload

--- JVChat_Premium.user.js
+++ JVChat_Premium.user.js
@@ -4,7 +4,7 @@
 // @author         Blaff, Rand0max, Atlantis/Lantea-Git
 // @namespace      JV_Chat_Custsom_Fork
 // @license        MIT
-// @version        0.2.3.88
+// @version        0.2.3.89
 // @icon           https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/128px/2b1b.png
 // @match          http://*.jeuxvideo.com/forums/42-*
 // @match          https://*.jeuxvideo.com/forums/42-*
@@ -3866,7 +3866,7 @@ function getPayload(doc) {
   try {
     const scriptPayLoad = [...doc.scripts].find(s => s.textContent?.includes('forumsAppPayload'))?.textContent;
     const rawPayload = scriptPayLoad?.match(/jvc\.forumsAppPayload\s*=\s*["']?([^"']+)["']?/)?.[1];
-    if (!rawPayload) return null;
+    if (!rawPayload) return undefined;
     try {
         return JSON.parse(atob(rawPayload)); //BASE 64
     } catch {
@@ -3874,7 +3874,7 @@ function getPayload(doc) {
     }
   } catch (e) {
       console.error("Erreur extraction du payload:", e);
-      return null;
+      return undefined;
   }
 }
 
