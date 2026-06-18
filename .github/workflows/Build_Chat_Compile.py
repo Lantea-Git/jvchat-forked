e58d7e642260fe01ce9013a24898382b7e668005
Atlantis <184443362+Lantea-Git@users.noreply.github.com>
2026-06-18 06:57:21 +0200

    Update Build_Chat_Compile.py

--- .github/workflows/Build_Chat_Compile.py
+++ .github/workflows/Build_Chat_Compile.py
@@ -24,6 +24,7 @@ new_header = f"""// ==UserScript==
 // @match          http://*.jeuxvideo.com/forums/1-*
 // @match          https://*.jeuxvideo.com/forums/1-*
 // @require        https://cdn.jsdelivr.net/npm/fflate@0.8.2/umd/index.js
+// @grant          none
 // @downloadURL https://update.greasyfork.org/scripts/576263/JV_Chat_Custsom_Fork.user.js
 // @updateURL https://update.greasyfork.org/scripts/576263/JV_Chat_Custsom_Fork.meta.js
 // ==/UserScript=="""
