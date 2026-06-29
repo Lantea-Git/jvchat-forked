25f3a8aea3355a545b809939947d9f28e3723717
GitHub Action <action@github.com>
2026-06-29 20:55:54 +0000

    retry 503 / 500 CDM en cas derreur au lieu d'attendre 60 sec

--- JVChat_Premium.user.js
+++ JVChat_Premium.user.js
@@ -4,7 +4,7 @@
 // @author         Blaff, Rand0max, Atlantis/Lantea-Git
 // @namespace      JV_Chat_Custsom_Fork
 // @license        MIT
-// @version        0.2.5.316
+// @version        0.2.5.321
 // @icon           https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/128px/2b1b.png
 // @match          http://*.jeuxvideo.com/forums/42-*
 // @match          https://*.jeuxvideo.com/forums/42-*
@@ -3416,7 +3416,8 @@ function updateMessages(page, goToLast) {
     }
 
     function onError(err, code) {
-        if (code === 0) {
+        //if (code === 0) {
+        if (code === 0 || code === 500 || code === 503 ) { //Erreur 500/3 intenpestives CDM TEMP.
             scheduleDegradedRefreshWarning();
             scheduleNextUpdate(turboActivated ? configuration["turbo_delay"] : 10000, page, true);
             return
