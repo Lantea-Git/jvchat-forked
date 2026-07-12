94a9942278c4af4d078762f5148164d7371051a6
GitHub Action <action@github.com>
2026-07-12 17:13:16 +0000

    force scroll down bug croisé plug-in externe

--- JVChat_Premium.user.js
+++ JVChat_Premium.user.js
@@ -4,7 +4,7 @@
 // @author         Blaff, Rand0max, Atlantis/Lantea-Git
 // @namespace      JV_Chat_Custsom_Fork
 // @license        MIT
-// @version        0.2.6.050
+// @version        0.2.6.100
 // @icon           https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/128px/2b1b.png
 // @match          http://*.jeuxvideo.com/forums/42-*
 // @match          https://*.jeuxvideo.com/forums/42-*
@@ -3110,7 +3110,11 @@ function addMessages(messages, editing, requestTimestamp) {
         }
         for (let newMessageId of newMessagesIds) {
             let event = new CustomEvent('jvchat:newmessage', { 'detail': { id: newMessageId, isEdit: false } });
+            let isDown = isScrollDown();
             dispatchEvent(event);
+            if (isDown) {
+                setScrollDown(); //Force un scrool down si un plugin externe modifie le dom
+            }
         }
     } else {
         if (!turboActivated && !refreshDegraded) {
