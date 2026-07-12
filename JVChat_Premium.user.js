34150d5f9941a6e9c0c7b5103d59728ff7a96acc
GitHub Action <action@github.com>
2026-07-12 15:26:45 +0000

    clean css

--- JVChat_Premium.user.js
+++ JVChat_Premium.user.js
@@ -4,7 +4,7 @@
 // @author         Blaff, Rand0max, Atlantis/Lantea-Git
 // @namespace      JV_Chat_Custsom_Fork
 // @license        MIT
-// @version        0.2.6.020
+// @version        0.2.6.050
 // @icon           https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/128px/2b1b.png
 // @match          http://*.jeuxvideo.com/forums/42-*
 // @match          https://*.jeuxvideo.com/forums/42-*
@@ -77,7 +77,6 @@ header.jv-header-menu,
 .option-previsu,
 .ads,
 .sondage-fofo,
-#forums-topic-survey,
 #dfp_pulse,
 #didomi-host,
 .sideDfp,
@@ -90,7 +89,6 @@ header.jv-header-menu,
 .gameHeaderBanner,
 .gameHeaderSubNav,
 .forumAnchorWrapper,
-.lockInfo,
 .bloc-sondage::before,
 .layout__contextTop,
 .layout__breadcrumb,
@@ -104,6 +102,10 @@ header.jv-header-menu,
 .layout__footer,
 .layout__contentTop,
 .header,
+/* New Respawn 2*/
+.toast,
+.lockInfo,
+#forums-topic-survey,
 .messageUser,
 .messageUser__anchor,
 .container__pagination,
@@ -128,7 +130,7 @@ header.jv-header-menu,
     display: none !important;
 }
 
-/* [NEW LAYOUT RESPAWN 2 MSG ]*/
+/* [NEW LAYOUT SCROLL RESPAWN 2 MSG ]*/
 
 #forum-main-col > .container__main:nth-last-of-type(2) {
     height: 100% !important;
