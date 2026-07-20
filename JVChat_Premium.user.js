54ff58e28b12787a5051e6d8d283fe50b20569e5
GitHub Action <action@github.com>
2026-07-20 18:21:08 +0000

    fix v410 noel:

--- JVChat_Premium.user.js
+++ JVChat_Premium.user.js
@@ -4,7 +4,7 @@
 // @author         Blaff, Rand0max, Atlantis/Lantea-Git
 // @namespace      JV_Chat_Custsom_Fork
 // @license        MIT
-// @version        0.2.6.500
+// @version        0.2.6.501
 // @icon           https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/128px/2b1b.png
 // @match          http://*.jeuxvideo.com/forums/42-*
 // @match          https://*.jeuxvideo.com/forums/42-*
@@ -1455,6 +1455,7 @@ let leavingTopic = false;
 let storageKey = "jvchat-premium-fork-configuration";
 let ringBell = undefined;
 let configuration = undefined;
+
 
 function defaultConfig() {
     return {
